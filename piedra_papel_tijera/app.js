3ded0572e7184856f496d6f5070d9bdd028961a4
AlvaroDaw2024 <alvarodaw2024@gmail.com>
2025-12-10 20:52:46 +0100

    refactor para integracion server

--- piedra_papel_tijera/app.js
+++ piedra_papel_tijera/app.js
@@ -1,27 +1,6 @@
 import "dotenv/config";
-import cors from "cors";
-import express from "express";
-import db from "./database/db.js";
-import usuarioRoutes from "./routes/usuarioRoutes.js";
-import partidasRoutes from "./routes/partidaRoutes.js";
-import authRoutes from "./routes/authRoutes.js";
-import "./models/asociaciones.js";
-const app = express();
+import MiServer from "../server";
 
-app.use(express.json());
+const server = new MiServer();
 
-app.use(cors());
-app.use("/usuarios", usuarioRoutes);
-app.use("/partidas", partidasRoutes);
-app.use("/", authRoutes);
-
-try {
-  await db.authenticate();
-  console.log("Oleole"); //ya que estoy dejo los mensajes, que me hacen gracia
-
-  app.listen(8000, () => {
-    console.log("escuchamos el 8000");
-  });
-} catch {
-  console.log("no va");
-}
+server.listen();
--- piedra_papel_tijera/controllers/SocketController.js
+++ piedra_papel_tijera/controllers/SocketController.js
@@ -1,20 +1,13 @@
-export const socketController = (io) => {
-  io.on("connection", (socket) => {
-    console.log("⚡ Cliente conectado ID:", socket.id);
+export const socketController = (socket, io) => {
+  console.log("Conectau", socket.id);
 
-    // EVENTO: Unirse a una sala de partida
-    // El cliente (frontend) emitirá: socket.emit("unirse_partida", 4);
-    socket.on("unirse_partida", (id_partida) => {
-      const sala = `partida_${id_partida}`;
-      socket.join(sala);
-      console.log(`Socket ${socket.id} se unió a la sala: ${sala}`);
+  socket.on("unirse_partida", (id_partida) => {
+    const sala = `partida_${id_partida}`;
+    socket.join(sala);
+    console.log(`Socket ${socket.id} se unió a la sala: ${sala}`);
+  });
 
-      io.to(sala).emit("usuario_unido", { mensaje: "Alguien ha entrado" });
-    });
-
-    // EVENTO: Desconexión
-    socket.on("disconnect", () => {
-      console.log("Adiossss", socket.id);
-    });
+  socket.on("disconnect", () => {
+    console.log("❌ Cliente desconectado ID:", socket.id);
   });
 };
