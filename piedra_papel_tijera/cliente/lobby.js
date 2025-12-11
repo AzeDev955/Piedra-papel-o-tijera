89463f5a2baa8b226d955fae8db3343a2db01599
AzeDev <alvarodaw2024@gmail.com>
2025-12-11 14:14:13 +0100

    funciones varias

--- piedra_papel_tijera/cliente/lobby.js
+++ piedra_papel_tijera/cliente/lobby.js
@@ -1,3 +1,4 @@
+import { crearHeader, construirApi } from "./helper/helper";
 const btnLogout = document.getElementById("btnLogout");
 const btnCrearPartida = document.getElementById("btnCrearPartida");
 const listaPartidas = document.getElementById("listaPartidas");
@@ -7,4 +8,19 @@ btnLogout.addEventListener("click", (e) => {
   e.preventDefault();
   localStorage.clear();
   window.location.href = "./login.html";
+});
+
+btnCrearPartida.addEventListener("click", async () => {
+  const endpoint = "/partidas/crear";
+  try {
+    const response = await fetch(
+      construirApi(endpoint),
+      crearHeader("POST", token)
+    );
+    if (!response.ok) {
+      console.log("Ayuda");
+    }
+  } catch (error) {
+    console.error(error);
+  }
 });
