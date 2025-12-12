01fe1a5b0677a581df019479d5e253584b5211a7
AlvaroDaw2024 <alvarodaw2024@gmail.com>
2025-12-12 12:15:02 +0100

    nickname en pagina

--- piedra_papel_tijera/cliente/lobby.js
+++ piedra_papel_tijera/cliente/lobby.js
@@ -3,6 +3,15 @@ const btnLogout = document.getElementById("btnLogout");
 const btnCrearPartida = document.getElementById("btnCrearPartida");
 const listaPartidas = document.getElementById("listaPartidas");
 const token = localStorage.getItem("token");
+const user_info = localStorage.getItem("usuario");
+const spanUsuario = document.getElementById("nombreUsuario");
+
+if (!token) {
+  alert("Identificate!");
+  window.location.href("./login.html");
+} else {
+  spanUsuario.innerHTML = user_info.nickname;
+}
 
 btnLogout.addEventListener("click", (e) => {
   e.preventDefault();
